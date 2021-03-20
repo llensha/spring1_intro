b16345f770f369d7a4c93cdd45d9814f2118c2ec
llensha <llensha@mail.ru>
2021-03-20 07:31:24 +0300

    Correct

--- zuul-server/src/main/java/ru/geekbrains/zuul/server/ZuulServerApplication.java
+++ zuul-server/src/main/java/ru/geekbrains/zuul/server/ZuulServerApplication.java
@@ -17,16 +17,4 @@ public class ZuulServerApplication {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("*")
-//                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-//            }
-//        };
-//    }
-
 }
